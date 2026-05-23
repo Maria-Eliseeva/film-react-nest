02be76de1227c7aa20968f09d3852b4cb22d8ee2
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-05-23 20:26:07 +0300

    модуль order

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
-
 import { configProvider } from './app.config.provider';
 import { databaseProvider } from './database/database.provider';
 import { FilmsModule } from './films/films.module';
+import { OrderModule } from './order/order.module';
 import { OrderService } from './order/order.service';
+import { FilmsService } from './films/films.service';
 import { OrderController } from './order/order.controller';
+import { FilmsController } from './films/films.controller';
 import { FilmsMongoDbRepository } from './repository/films/films';
 
 @Module({
@@ -21,12 +23,14 @@ import { FilmsMongoDbRepository } from './repository/films/films';
       serveRoot: '/content/afisha',
     }),
     FilmsModule,
+    OrderModule,
   ],
-  controllers: [OrderController],
+  controllers: [OrderController, FilmsController],
   providers: [
     configProvider,
     databaseProvider,
     OrderService,
+    FilmsService,
     FilmsMongoDbRepository,
   ],
 })
