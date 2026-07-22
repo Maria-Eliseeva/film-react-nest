ff8c33269985c0c07d3a0bdc21bbc26b61cc3d0b
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-07-22 23:23:09 +0300

    логгеры

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config.provider';
-
+import { TskvLogger } from './logger/TskvLogger';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-
+  app.useLogger(new TskvLogger());
   const config = app.get<AppConfig>('CONFIG');
   await app.listen(config.port);
 }
