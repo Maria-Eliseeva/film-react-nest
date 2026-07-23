2c1a48ee15cb86f79d4d590d0332b16cada3d1fd
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-07-23 16:28:21 +0300

    fix пути front

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? '/api/afisha';
+const defaultApiUrl = import.meta.env.PROD
+  ? 'https://api.film.mary.nomorepartiessite.ru/api/afisha'
+  : '/api/afisha';
+
+export const API_URL = import.meta.env.VITE_API_URL ?? defaultApiUrl;
 export const CDN_URL = import.meta.env.VITE_CDN_URL ?? '/content/afisha';
